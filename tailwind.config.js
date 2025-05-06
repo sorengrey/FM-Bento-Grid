f6f1199bb2031e432d31cde31b3fcb9df6104f0f
Heather Smith <halexsmith86@gmail.com>
2025-05-06 14:59:31 -0400

    Added gitignore, fixed classes
    Fixed classes, tailwind config, added gitignore

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+// /** @type {import('tailwindcss').Config} */
+tailwind.config = {
+  // content: [
+  //   "./index.html", // Or wherever your HTML files are
+  //   // "./src/**/*.{html,js}", // If you're using a source folder
+  // ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['"DM Sans"', "sans-serif"],
+      },
+      screens: {
+        sm: "376px",
+        md: "769px",
+        lg: "1439px",
+      },
+      colors: {
+        background: "#FFFFFF",
+        foreground: "#121212",
+        "purple-100": "#DBD1FC",
+        "purple-500": "#7551DC",
+        "yellow-100": "#F9EEE2",
+        "yellow-500": "#FFCC6A",
+        black: "#121212",
+        white: "#FFFFFF",
+      },
+    },
+  },
+  plugins: [],
+};
